c381d6b8cc799c5098a317174ef538a196dcbe71
nivedita@jobvite-inc.com <nivedita@jobvite-inc.com>
2018-12-11 14:28:52 +0530

    Hire-18886

---
+++ HIRE-18886_RB_UpdateRecruiterForReqsListed.sql
@@ -0,0 +1,189 @@
+BEGIN TRAN
+	--Rollback T_JobOwner
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588043
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588294
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588050
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588015
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588011
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588009
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588004
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588002
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18587998
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18587993
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18587984
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18588506
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18594111
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18595751
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18596861
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19932970
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18596650
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18596717
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18596719
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18596723
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19748768
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22625125
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18867898
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 18793625
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19740482
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19740690
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19740788
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19915413
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19740763
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19740730
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 19468192
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22625095
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23189908
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21971401
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22624745
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21389454
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 20504431
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 20602093
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 20631985
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 20783298
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 22070860
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22624713
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 20899748
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 22071006
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21508848
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21125001
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22624708
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21077429
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23533103
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21126158
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21994968
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21554935
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22226842
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22624702
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22624861
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21259653
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21487349
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21340260
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22980976
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21512912
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21418695
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22652659
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21509162
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21669311
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21737370
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21810509
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21810618
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 21878703
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23636913
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 22023641
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22900321
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23029392
+	Update T_JobOwner Set UserId = 31365714 Where CompanyId = 5634 And PkId = 22202091
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22350628
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22298221
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22347084
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22625059
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22456391
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22456424
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22489735
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22509627
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22606227
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22608436
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22677203
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22722956
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22873375
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 22931469
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23031120
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23133897
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23294924
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23295035
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23342112
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23414977
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23426122
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23440683
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23456402
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23558825
+	Update T_JobOwner Set UserId = 43151882 Where CompanyId = 5634 And PkId = 23583372
+
+	--Rollback T_Job PrimaryRecruiterId
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '02/10/2018 00:22:12' Where CompanyId = 5634 And Id = 2653709
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '03/06/2018 03:41:07' Where CompanyId = 5634 And Id = 2770591
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '05/03/2018 21:50:20' Where CompanyId = 5634 And Id = 2780070
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '02/10/2018 00:34:38' Where CompanyId = 5634 And Id = 2788239
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/27/2018 02:55:26' Where CompanyId = 5634 And Id = 2790546
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/26/2018 21:25:34' Where CompanyId = 5634 And Id = 2790564
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '03/06/2018 03:41:28' Where CompanyId = 5634 And Id = 2790575
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/28/2018 00:07:09' Where CompanyId = 5634 And Id = 2817491
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:37:07' Where CompanyId = 5634 And Id = 2817626
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '02/28/2018 21:25:04' Where CompanyId = 5634 And Id = 2819574
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '02/23/2018 02:29:52' Where CompanyId = 5634 And Id = 2826261
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/23/2018 12:16:50' Where CompanyId = 5634 And Id = 2833615
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/23/2018 12:22:41' Where CompanyId = 5634 And Id = 2833714
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/23/2018 12:25:30' Where CompanyId = 5634 And Id = 2835631
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '05/03/2018 07:11:02' Where CompanyId = 5634 And Id = 2845004
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/23/2018 12:24:46' Where CompanyId = 5634 And Id = 2848932
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/23/2018 12:23:57' Where CompanyId = 5634 And Id = 2855416
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '04/05/2018 22:24:22' Where CompanyId = 5634 And Id = 2870735
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:29:38' Where CompanyId = 5634 And Id = 2902175
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/08/2018 05:50:10' Where CompanyId = 5634 And Id = 2902176
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/31/2018 02:31:38' Where CompanyId = 5634 And Id = 2920186
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:11:05' Where CompanyId = 5634 And Id = 2967993
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/27/2018 03:21:37' Where CompanyId = 5634 And Id = 2980146
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/05/2018 08:43:05' Where CompanyId = 5634 And Id = 3003929
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '06/11/2018 23:35:44' Where CompanyId = 5634 And Id = 3012682
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '06/13/2018 05:01:30' Where CompanyId = 5634 And Id = 3015350
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '10/24/2018 23:03:01' Where CompanyId = 5634 And Id = 3015375
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '10/24/2018 23:03:49' Where CompanyId = 5634 And Id = 3021326
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:05:59' Where CompanyId = 5634 And Id = 3040622
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '06/27/2018 22:55:22' Where CompanyId = 5634 And Id = 3042376
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '09/07/2018 03:05:24' Where CompanyId = 5634 And Id = 3050792
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/02/2018 23:28:00' Where CompanyId = 5634 And Id = 3052643
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/12/2018 05:13:18' Where CompanyId = 5634 And Id = 3052705
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:05:41' Where CompanyId = 5634 And Id = 3052808
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/29/2018 23:33:42' Where CompanyId = 5634 And Id = 3062111
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/28/2018 04:14:14' Where CompanyId = 5634 And Id = 3066175
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/30/2018 04:51:59' Where CompanyId = 5634 And Id = 3066323
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '10/31/2018 00:17:19' Where CompanyId = 5634 And Id = 3070142
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '10/31/2018 00:17:30' Where CompanyId = 5634 And Id = 3070163
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '09/17/2018 00:14:07' Where CompanyId = 5634 And Id = 3070213
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/31/2018 00:17:33' Where CompanyId = 5634 And Id = 3070287
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:12:53' Where CompanyId = 5634 And Id = 3072318
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/19/2018 21:59:55' Where CompanyId = 5634 And Id = 3077907
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/02/2018 01:46:34' Where CompanyId = 5634 And Id = 3080398
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/25/2018 02:09:34' Where CompanyId = 5634 And Id = 3084255
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/28/2018 22:39:58' Where CompanyId = 5634 And Id = 3085966
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/15/2018 22:02:46' Where CompanyId = 5634 And Id = 3090229
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '07/30/2018 06:59:07' Where CompanyId = 5634 And Id = 3090478
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/10/2018 05:27:30' Where CompanyId = 5634 And Id = 3091968
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/03/2018 00:21:55' Where CompanyId = 5634 And Id = 3098571
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/13/2018 21:59:13' Where CompanyId = 5634 And Id = 3113502
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/16/2018 23:36:01' Where CompanyId = 5634 And Id = 3119481
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/22/2018 00:19:36' Where CompanyId = 5634 And Id = 3125899
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/22/2018 00:37:51' Where CompanyId = 5634 And Id = 3125924
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '08/27/2018 02:19:34' Where CompanyId = 5634 And Id = 3131858
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '12/06/2018 00:30:07' Where CompanyId = 5634 And Id = 3139421
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '10/02/2018 22:40:10' Where CompanyId = 5634 And Id = 3146019
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/23/2018 22:05:59' Where CompanyId = 5634 And Id = 3148375
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/31/2018 00:19:01' Where CompanyId = 5634 And Id = 3150086
+	Update T_Job Set PrimaryRecruiterId = 31365714, Modified = '09/14/2018 04:43:37' Where CompanyId = 5634 And Id = 3164974
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/09/2018 04:49:31' Where CompanyId = 5634 And Id = 3172164
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '09/19/2018 21:31:15' Where CompanyId = 5634 And Id = 3173980
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '09/23/2018 22:49:47' Where CompanyId = 5634 And Id = 3179286
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 22:19:43' Where CompanyId = 5634 And Id = 3192391
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '09/28/2018 04:14:27' Where CompanyId = 5634 And Id = 3192448
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/24/2018 22:00:10' Where CompanyId = 5634 And Id = 3192449
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/08/2018 20:25:42' Where CompanyId = 5634 And Id = 3196474
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/01/2018 18:25:37' Where CompanyId = 5634 And Id = 3198536
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/09/2018 02:17:49' Where CompanyId = 5634 And Id = 3206811
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/08/2018 06:43:04' Where CompanyId = 5634 And Id = 3206994
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/23/2018 22:43:22' Where CompanyId = 5634 And Id = 3211182
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/14/2018 21:37:57' Where CompanyId = 5634 And Id = 3217200
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/22/2018 21:33:56' Where CompanyId = 5634 And Id = 3231024
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/25/2018 06:57:11' Where CompanyId = 5634 And Id = 3236202
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '10/31/2018 03:14:41' Where CompanyId = 5634 And Id = 3244782
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/05/2018 23:12:12' Where CompanyId = 5634 And Id = 3251650
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/14/2018 03:53:21' Where CompanyId = 5634 And Id = 3268751
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/14/2018 04:08:01' Where CompanyId = 5634 And Id = 3268759
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/15/2018 22:52:01' Where CompanyId = 5634 And Id = 3272687
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/19/2018 23:57:58' Where CompanyId = 5634 And Id = 3277851
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/20/2018 10:17:05' Where CompanyId = 5634 And Id = 3278758
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/21/2018 01:56:25' Where CompanyId = 5634 And Id = 3280122
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/21/2018 20:20:12' Where CompanyId = 5634 And Id = 3281509
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/29/2018 06:43:30' Where CompanyId = 5634 And Id = 3289537
+	Update T_Job Set PrimaryRecruiterId = 43151882, Modified = '11/30/2018 03:35:42' Where CompanyId = 5634 And Id = 3291502
+
+COMMIT
