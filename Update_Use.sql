02b15a44bcb1a501fdcc50084dcf0aa76fb9a383
veenarajendran <veena@jobvite-inc.com>
2019-01-08 01:42:08 -0800

    TAS-2321 Updating closed date

---
+++ Update_Use.sql
@@ -0,0 +1,39 @@
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-06')
+ WHERE CompanyId=6794 and JobId=3306177
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2015-11-24')
+ WHERE CompanyId=6794 and JobId=3306180
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-22')
+ WHERE CompanyId=6794 and JobId=3306181
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-11')
+ WHERE CompanyId=6794 and JobId=3306182
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-05-29')
+ WHERE CompanyId=6794 and JobId=3306184
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2017-08-10')
+ WHERE CompanyId=6794 and JobId=3306187
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2017-10-20')
+ WHERE CompanyId=6794 and JobId=3306188
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2018-03-23')
+ WHERE CompanyId=6794 and JobId=3306190
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2018-06-19')
+ WHERE CompanyId=6794 and JobId=3306191
+
+
+
