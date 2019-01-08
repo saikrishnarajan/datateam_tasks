e918d9ddd63f44c837251972aa6746ba9bdb4da2
veenarajendran <veena@jobvite-inc.com>
2019-01-08 01:57:28 -0800

    TAS-2321 - Updating remianinf closed date

---
+++ Update_Use1.sql
@@ -0,0 +1,39 @@
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-06' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306177
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2015-11-24' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306180
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-22' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306181
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-01-11' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306182
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2016-05-29' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306184
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2017-08-10' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306187
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2017-10-20' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306188
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2018-03-23' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306190
+
+UPDATE [T_JobOpen]
+   SET [ToDate] = CAST('2018-06-19' AS Datetime)
+ WHERE CompanyId=6794 and JobId=3306191
+
+
+
